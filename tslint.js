7a5ec7c2c5274ecc3dc420165d4498be9b084ef1
Ricardo Javier Barrios Díaz <ricardojbarrios@gmail.com>
2018-05-27 17:13:55 +0100

    feat(rules): wip

--- tslint.js
+++ tslint.js
@@ -8,31 +8,59 @@ module.exports = {
   ],
   rules: {
     /**************************************************************************
-     * Class and Member design
+     * Class and member design
      *************************************************************************/
     "adjacent-overload-signatures": true,
+    "member-access": true,
+    "member-ordering": [
+      true,
+      "public-before-private",
+      "static-before-instance",
+      "variables-before-functions"
+    ],
+
     /**************************************************************************
-     * Basic Types
+     * Interface design
      *************************************************************************/
+    "no-empty-interface": true,
+
+    /**************************************************************************
+     * Types
+     *************************************************************************/
+    // "no-inferrable-types": false, // Always use types
+
+    /*** Basic types *********************************************************/
     "ban-types": [
       true,
-      ["Object", "Use {} instead."],
-      ["object", "Use {} instead."],
+      ["Object", "Use the empty object type {} instead."],
+      ["object", "Use the empty object type {} instead."],
       ["String", "Use string instead."],
       ["Number", "Use number instead."],
       ["Boolean", "Use boolean instead."]
     ],
-    /**************************************************************************
-     * Collections
-     *************************************************************************/
+    "no-any": true,
+    "no-non-null-assertion": true,
+
+    /*** Collections *********************************************************/
     "array-type": [true, "array"],
+    "no-array-delete": true,
+    "no-collection-size-mischeck": true,
+    "no-element-overwrite": true,
     "no-sparse-arrays": true,
     // "ter-no-sparse-arrays": [true] // Duplicate of no-sparse-arrays
-    "no-array-delete": true,
-    "no-element-overwrite": true,
-    "no-collection-size-mischeck": true
+
     /**************************************************************************
-     * Comments and documentation
+     * Assignments, variables and constants
      *************************************************************************/
+    "no-magic-numbers": true,
+    "no-parameter-reassignment": true,
+
+    /**************************************************************************
+     * Imports
+     *************************************************************************/
+    "no-import-side-effect": true,
+    "no-internal-module": true,
+    "no-namespace": true,
+    "no-reference": true
   }
 };
